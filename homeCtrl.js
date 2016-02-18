33f5ba66f6ae79e616cc19ea7fe7a3c2cdcca6d0
Mike <mikeytown19@gmail.com>
2016-02-18 14:54:26 -0700

    finished

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,55 +1,77 @@
-angular.module('devMtIn').controller('homeCtrl', function($scope, profileService){
+angular.module('devMtIn')
+.controller('homeCtrl', function( $scope, profileService, friendService ) {
+	$scope.editing = false;
 
+	$scope.saveProfile = function( profile ) {
+		profileService.saveProfile(profile);
+		$scope.editing = false;
+	}
 
-$scope.editing = false;
+	$scope.deleteProfile = function() {
+		profileService.deleteProfile()
+			.then(function( deletedProfile ) {
+				localStorage.removeItem('profileId');
+				$scope.myProfile = {};
+			})
+			.catch(function( err ) {
+				console.error(err);
+			});
+	}
 
-profileService.serviceTest();
+	$scope.checkForProfile = function() {
+		var profileId = JSON.parse(localStorage.getItem('profileId'));
 
-$scope.checkForProfile = function() {
-  var profileId = JSON.parse(localStorage.getItem('profileId'));
+		if (profileId) {
+			profileService.checkForProfile(profileId.profileId)
+				.then(function( profile ) {
+					$scope.myProfile = profile.data;
+					friendService.findFriendsFriends(profile.data);
+				})
+				.catch(function( err ) {
+					console.error(err);
+				});
+		}
+	}
+	$scope.checkForProfile();
 
-  if (profileId) {
-    profileService.checkForProfile(profileId.profileId)
-    .then(function(profile) {
-      $scope.myProfile = profile.data;
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
-  }
-}
+	$scope.findFriends = function( query ) {
+		friendService.findFriends($scope.myProfile._id, query)
+			.then(function( potentialFriends ) {
+				$scope.potentialFriends = potentialFriends;
+			})
+			.catch(function( err ) {
+				return console.error(err);
+			});
+	}
 
-$scope.saveProfile = function('profile'){
-  profileService.saveProfile(profile);
-  $scope.editing = false;
-};
+	$scope.addFriend = function( friendId ) {
+		friendService.addFriend($scope.myProfile._id, friendId)
+			.then(function( profile ) {
+				$scope.checkForProfile();
+			})
+			.catch(function( err ) {
+				return console.error(err);
+			});
+	}
 
-$scope.deleteProfile = function() {
-  profileService.deleteProfile()
-  .then(function(deletedProfile) {
-    localStorage.removeItem('profileId');
-    $scope.myProfile = {};
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-}
+	$scope.removeFriend = function( friendId ) {
+		friendService.removeFriend($scope.myProfile._id, friendId)
+			.then(function( profile ) {
+				$scope.checkForProfile();
+			})
+			.catch(function( err ) {
+				console.error(err);
+			});
+	}
 
-profileService.deleteProfile
-  $scope.myProfile = {
+	$scope.sortOptions = [{
+		  display: 'Ascending'
+		, value: false
+	},
+	{
+		  display: 'Descending'
+		, value: true
+	}
+	];
 
-    friends:[
-      {name:"ryan"},{name:"scott"},{name:"jake"},{name:"ben"},{name:"brad"},
-    ],
-  }
-
-  $scope.sortOptions = [{
-      display: 'Ascending'
-    , value: false
-    },
-    {
-      display: 'Descending'
-    , value: true
-    }
-  ];
 });
--- profileService.js
+++ profileService.js
@@ -1,37 +1,35 @@
-angular.module('devMtIn').service('profileService', $http, function() {
+angular.module('devMtIn')
+.service('profileService', function( $http ) {
+	var baseUrl = 'http://localhost:8081';
 
-  var baseUrl = 'http://connections.devmounta.in/';
+	this.saveProfile = function( profile ) {
+		$http({
+			  method: 'POST'
+			, url: baseUrl + '/api/profiles'
+			, data: profile
+		})
+			.then(function( profileResponse ) {
+				localStorage.setItem('profileId', JSON.stringify({ profileId: profileResponse.data._id }));
+			})
+			.catch(function( err ) {
+				console.error(err);
+			});
+	}
 
-  this.saveProfile = function(profile) {
-    return $http({
-        method: 'POST' // Request method.
-          ,
-        url: baseUrl + 'api/profiles/' // URL we are making the request to.
-          ,
-        data: profile // The data we are requesting be posted.
-      }).then(function(profileResponse) { // What to do after a response comes back from the server.
-        localStorage.setItem('profileId', JSON.stringify({
-          profileId: profileResponse.data._id
-        })); // Save our unique _id to local storage
-      })
-      .catch(function(err) {
-        console.error(err);
-      });
-  }
-  this.deleteProfile = function() {
-    var profileId = JSON.parse(localStorage.getItem('profileId')).profileId;
+	this.checkForProfile = function( profileId ) {
+		return $http({
+			  method: 'GET'
+			, url: baseUrl + '/api/profiles/' + profileId
+		});
+	}
 
-    return $http({
-      method: 'DELETE',
-      url: baseUrl + 'api/profiles/' + profileId
-    });
-  }
-  this.checkForProfile = function(profileId) {
-    return $http({
-      method: 'GET',
-      url: baseUrl + 'api/profiles/' + profileId
-    });
-  }
+	this.deleteProfile = function() {
+		var profileId = JSON.parse(localStorage.getItem('profileId')).profileId;
 
+		return $http({
+			  method: 'DELETE'
+			, url: baseUrl + '/api/profiles/' + profileId
+		});
+	}
 
-})
+});
